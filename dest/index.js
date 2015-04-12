a6cd58bbc8763dc4cd3898798a48fdb43ec001a9
Szymon Wygnanski <s@finalclass.net>
2015-04-12 21:23:13 +0200

    adding requrie-dir to devDependencies

---
+++ dest/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var RestRouter = function RestRouter() {
+  _classCallCheck(this, RestRouter);
+
+  this.test = 'ok';
+};
+
+exports['default'] = RestRouter;
+module.exports = exports['default'];
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O0lBQU0sVUFBVSxHQUNILFNBRFAsVUFBVSxHQUNBO3dCQURWLFVBQVU7O0FBRVosTUFBSSxDQUFDLElBQUksR0FBRyxJQUFJLENBQUM7Q0FDbEI7O3FCQUdZLFVBQVUiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiLy9ob21lL3NlbC9Eb2N1bWVudHMvc3Ryb2plL3JvYm9sL25vZGVfbW9kdWxlcy9iZWFtLXJlc3Qtcm91dGVyL3NyYyJ9
---
+++ test/dest/index.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _interopRequireWildcard = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
+
+var _RestRouter = require('../../dest/index');
+
+var _RestRouter2 = _interopRequireWildcard(_RestRouter);
+
+describe('RestRouter', function () {
+  it('is ok', function () {
+    var restRouter = new _RestRouter2['default']();
+    expect(restRouter.test).toBe('ok');
+  });
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LnNwZWMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OzswQkFBdUIsa0JBQWtCOzs7O0FBRXpDLFFBQVEsQ0FBQyxZQUFZLEVBQUUsWUFBTTtBQUMzQixJQUFFLENBQUMsT0FBTyxFQUFFLFlBQU07QUFDaEIsUUFBSSxVQUFVLEdBQUcsNkJBQWdCLENBQUM7QUFDbEMsVUFBTSxDQUFDLFVBQVUsQ0FBQyxJQUFJLENBQUMsQ0FBQyxJQUFJLENBQUMsSUFBSSxDQUFDLENBQUM7R0FDcEMsQ0FBQyxDQUFDO0NBQ0osQ0FBQyxDQUFDIiwiZmlsZSI6ImluZGV4LnNwZWMuanMiLCJzb3VyY2VSb290IjoiLy9ob21lL3NlbC9Eb2N1bWVudHMvc3Ryb2plL3JvYm9sL25vZGVfbW9kdWxlcy9iZWFtLXJlc3Qtcm91dGVyL3Rlc3Qvc3JjIn0=
